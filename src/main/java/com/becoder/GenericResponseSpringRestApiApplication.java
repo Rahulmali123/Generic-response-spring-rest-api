deb553ef6bfb8e106136566299adb1de643c63b7
apna-colleage-demo <rahulmali2140@gmail.com>
2024-09-26 19:02:12 +0530

    first commit

---
+++ src/main/java/com/becoder/GenericResponseSpringRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.becoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericResponseSpringRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericResponseSpringRestApiApplication.class, args);
+	}
+
+}
